5ae7feda7f3046e9446d7ac7812dc831ec836145
Jason Simms <simmsthecoder@gmail.com>
2024-02-13 15:50:46 +0100

    added tests for symbol validation

---
+++ src/__tests__/isSymbolValid.test.ts
@@ -0,0 +1,32 @@
+import { isSymbolValid, getSymbolInfo } from '../utils/isSymbolValid';
+
+describe('isSymbolValid', () => {
+  it('should return true for a valid symbol', () => {
+    expect(isSymbolValid('AAPL')).toBe(true);
+  });
+
+  it('should return false for an invalid symbol', () => {
+    expect(isSymbolValid('INVALIDSYMBOL')).toBe(false);
+  });
+});
+
+describe('getSymbolInfo', () => {
+  it('should return the correct stock object for a valid symbol', () => {
+    const expectedStock = {
+        "Symbol": "AAPL",
+        "Name": "Apple Inc. Common Stock",
+        "Market Cap": "2889948029150",
+        "Country": "United States",
+        "IPO Year": "1980",
+        "Volume": "41781934",
+        "Sector": "Technology",
+        "Industry": "Computer Manufacturing",
+        "": ""
+      };
+    expect(getSymbolInfo('AAPL')).toEqual(expectedStock);
+  });
+
+  it('should return undefined for an invalid symbol', () => {
+    expect(getSymbolInfo('INVALIDSYMBOL')).toBeUndefined();
+  });
+});
